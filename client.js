12f6ab061db92f448e2fab717caf6d46448fcf6c
xjzh123 <37945610+xjzh123@users.noreply.github.com>
2022-05-27 10:24:56 +0800

    update

--- client.js
+++ client.js
@@ -112,37 +112,9 @@ var verifyNickname = function (nick) {
 }
 
 var frontpage = [
-	"                            _           _         _       _   ",
-	"                           | |_ ___ ___| |_   ___| |_ ___| |_ ",
-	"                           |   |_ ||  _| '_| |  _|   |_ ||  _|",
-	"                           |_|_|__/|___|_,_|.|___|_|_|__/|_|  ",
-	"",
-	"",
-	"Welcome to hack.chat, a minimal, distraction-free chat application.",
-	"Channels are created, joined and shared with the url, create your own channel by changing the text after the question mark.",
-	"If you wanted your channel name to be 'your-channel': https://hack.chat/?your-channel",
-	"There are no channel lists, so a secret channel name can be used for private discussions.",
-	"",
-	"Here are some pre-made channels you can join:",
-	"?lounge ?meta",
-	"?math ?physics ?chemistry",
-	"?technology ?programming",
-	"?games ?banana",
-	"And here's a random one generated just for you: ?" + Math.random().toString(36).substr(2, 8),
-	"",
-	"Formatting:",
-	"Whitespace is preserved, so source code can be pasted verbatim.",
-	"Surround LaTeX with a dollar sign for inline style $\\zeta(2) = \\pi^2/6$, and two dollars for display. $$\\int_0^1 \\int_0^1 \\frac{1}{1-xy} dx dy = \\frac{\\pi^2}{6}$$",
-	"For syntax highlight, wrap the code like: ```<language> <the code>``` where <language> is any known programming language.",
-	"",
-	"Current Github: https://github.com/hack-chat",
-	"Legacy GitHub: https://github.com/AndrewBelt/hack.chat",
-	"",
-	"Bots, Android clients, desktop clients, browser extensions, docker images, programming libraries, server modules and more:",
-	"https://github.com/hack-chat/3rd-party-software-list",
-	"",
-	"Server and web client released under the WTFPL and MIT open source license.",
-	"No message history is retained on the hack.chat server."
+	"meow chat is a trash",
+	"just trash :)",
+	"or check [github](https://github.com/xjzh123/meow)"
 ].join("\n");
 
 function $(query) {
